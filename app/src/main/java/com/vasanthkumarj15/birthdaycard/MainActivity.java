d449a379658f98f22982d03fcff1b52d4d78c448
Vasanth Kumar <vasanthkumar.j@outlook.com>
2019-05-09 00:54:44 +0530

    second commit

--- app/src/main/java/com/vasanthkumarj15/birthdaycard/MainActivity.java
+++ app/src/main/java/com/vasanthkumarj15/birthdaycard/MainActivity.java
@@ -2,6 +2,9 @@ package com.vasanthkumarj15.birthdaycard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView zoom = (ImageView) findViewById(R.id.birthday_card);
+
+        final Animation zoomAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom);
+        zoom.startAnimation(zoomAnimation);
     }
 }
